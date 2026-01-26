d304eff2fab2f149efbd5fd31a2f4386a64f16cb
DESKTOP-D5TPFI4\smartts01 <giovanniadev@gmail.com>
2026-01-26 08:06:15 -0300

    Feat: novas features

--- RestWithASPNET10Erudio.Rest/Data/DTO/V2/PersonDTO.cs
+++ RestWithASPNET10Erudio.Rest/Data/DTO/V2/PersonDTO.cs
@@ -1,16 +1,29 @@
+using System.Text.Json.Serialization;
+using RestWithASPNET10Erudio.Rest.Utils;
+
 namespace RestWithASPNET10Erudio.Rest.Data.DTO.V2;
 
 public class PersonDTO
 {
+    //[JsonPropertyOrder(3)]
+    //[JsonPropertyName("code")]
     public long Id { get; set; }
     
+    //[JsonPropertyOrder(4)]
+    //[JsonPropertyName("first_name")]
     public string FirstName { get; init; }
     
+    //[JsonPropertyOrder(5)]
+    //[JsonPropertyName("last_name")]
     public string LastName { get; init; }
-    
+
+    //[JsonPropertyOrder(1)]
     public string Address { get; init; }
     
+    //[JsonPropertyOrder(6)]
     public string Gender { get; init; }
     
+    //[JsonPropertyOrder(2)]
+    [JsonConverter(typeof(DateSerializer))]
     public DateTime? BirthDay { get; init; }
 }
---
+++ RestWithASPNET10Erudio.Rest/JsonSerializers/GenderSerializer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace RestWithASPNET10Erudio.Rest.Utils;
+
+public class GenderSerializer : JsonConverter<String>
+{
+    
+    
+    public override void WriteJson(JsonWriter writer, string? value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string? ReadJson(
+        JsonReader reader, 
+        Type typeToConvert, 
+        string? existingValue, 
+        bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
